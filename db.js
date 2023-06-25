4bdeedf82bc97f7d12721a823498fd2f75bfd890
Arif Rahman <macbookpro2020@Arif-MacBook-Pro.local>
2023-06-25 23:49:57 +0600

    first commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+// class DatabaseConnection {
+//     constructor(){
+//         this.db = null
+//         this.dbURL = path.resolve(process.env.DB_URL)
+       
+//     }
+// }
+console.log(process.env.DB_URL)
+console.log(path.resolve(process.env.DB_URL))
+console.log('hello')
+
+
+
+
+
